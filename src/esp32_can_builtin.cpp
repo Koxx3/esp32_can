c0842aa3c6ed2ba7960f695e53f0ae2f57431ef3
Koxx3 <koxx33@gmail.com>
2023-01-25 23:41:47 +0100

    add traces

--- src/esp32_can_builtin.cpp
+++ src/esp32_can_builtin.cpp
@@ -1,8 +1,8 @@
 /*
   ESP32_CAN.cpp - Library for ESP32 built-in CAN module
-  
+
   Author: Collin Kidder
-  
+
   Created: 31/1/18
 */
 
@@ -14,8 +14,7 @@ CAN_device_t CAN_cfg = {
     GPIO_NUM_17,
     GPIO_NUM_16,
     NULL,
-    NULL
-};
+    NULL};
 
 QueueHandle_t callbackQueue;
 extern QueueHandle_t lowLevelRXQueue;
@@ -26,13 +25,15 @@ volatile bool rxFault2;
 volatile bool txFault2;
 volatile uint8_t faulted2;
 volatile uint8_t rxProcQueueFull = 0;
+volatile uint32_t txQueueAvail = 9999;
+volatile uint32_t rxQueueAvail = 9999;
 
 ESP32CAN::ESP32CAN(gpio_num_t rxPin, gpio_num_t txPin) : CAN_COMMON(32)
 {
     CAN_cfg.rx_pin_id = rxPin;
     CAN_cfg.tx_pin_id = txPin;
     cyclesSinceTraffic = 0;
-    rxBufferSize = BI_RX_BUFFER_SIZE; //set defaults
+    rxBufferSize = BI_RX_BUFFER_SIZE; // set defaults
     txBufferSize = BI_TX_BUFFER_SIZE;
 }
 
@@ -42,39 +43,39 @@ void ESP32CAN::setCANPins(gpio_num_t rxPin, gpio_num_t txPin)
     CAN_cfg.tx_pin_id = txPin;
 }
 
-void CAN_WatchDog_Builtin( void *pvParameters )
+void CAN_WatchDog_Builtin(void *pvParameters)
 {
-    ESP32CAN* espCan = (ESP32CAN*)pvParameters;
+    ESP32CAN *espCan = (ESP32CAN *)pvParameters;
     const TickType_t xDelay = 200 / portTICK_PERIOD_MS;
 
-     for(;;)
-     {
-        vTaskDelay( xDelay );
+    for (;;)
+    {
+        vTaskDelay(xDelay);
         espCan->cyclesSinceTraffic++;
         if (needReset)
         {
             espCan->cyclesSinceTraffic = 0;
             needReset = 0;
             faulted = false;
-            if (CAN_cfg.speed > 0 && CAN_cfg.speed <= 1000000ul && espCan->initializedResources == true) 
+            if (CAN_cfg.speed > 0 && CAN_cfg.speed <= 1000000ul && espCan->initializedResources == true)
             {
                 Serial.println(F("CAN_WatchDog_Builtin / Builtin CAN Forced Reset!"));
                 CAN_stop();
                 CAN_init();
             }
         }
-     }
+    }
 }
 
 void task_LowLevelRX(void *pvParameters)
 {
-    ESP32CAN* espCan = (ESP32CAN*)pvParameters;
+    ESP32CAN *espCan = (ESP32CAN *)pvParameters;
     CAN_frame_t rxFrame;
-    
+
     while (1)
     {
-        //receive next CAN frame from queue and fire off the callback
-        if(xQueueReceive(lowLevelRXQueue, &rxFrame, portMAX_DELAY)==pdTRUE)
+        // receive next CAN frame from queue and fire off the callback
+        if (xQueueReceive(lowLevelRXQueue, &rxFrame, portMAX_DELAY) == pdTRUE)
         {
             espCan->processFrame(rxFrame);
         }
@@ -91,15 +92,15 @@ bit   31 -    If set indicates an object callback
 bits  24-30 - Idx into listener table
 bits  0-7   - Mailbox number that triggered callback
 */
-void task_CAN( void *pvParameters )
+void task_CAN(void *pvParameters)
 {
-    ESP32CAN* espCan = (ESP32CAN*)pvParameters;
+    ESP32CAN *espCan = (ESP32CAN *)pvParameters;
     CAN_FRAME rxFrame;
 
     while (1)
     {
-        //receive next CAN frame from queue and fire off the callback
-        if(xQueueReceive(callbackQueue, &rxFrame, portMAX_DELAY)==pdTRUE)
+        // receive next CAN frame from queue and fire off the callback
+        if (xQueueReceive(callbackQueue, &rxFrame, portMAX_DELAY) == pdTRUE)
         {
             espCan->sendCallback(&rxFrame);
         }
@@ -108,28 +109,31 @@ void task_CAN( void *pvParameters )
 
 void ESP32CAN::sendCallback(CAN_FRAME *frame)
 {
-    //frame buffer
+    // frame buffer
     CANListener *thisListener;
     int mb;
     int idx;
 
     mb = (frame->fid & 0xFF);
-    if (mb == 0xFF) mb = -1;
+    if (mb == 0xFF)
+        mb = -1;
 
-    if (frame->fid & 0x80000000ul) //object callback
+    if (frame->fid & 0x80000000ul) // object callback
     {
         idx = (frame->fid >> 24) & 0x7F;
         thisListener = listener[idx];
         thisListener->gotFrame(frame, mb);
     }
-    else //C function callback
+    else // C function callback
     {
-        if (mb > -1) (*cbCANFrame[mb])(frame);
-        else (*cbGeneral)(frame);
+        if (mb > -1)
+            (*cbCANFrame[mb])(frame);
+        else
+            (*cbGeneral)(frame);
     }
 }
 
-ESP32CAN::ESP32CAN() : CAN_COMMON(BI_NUM_FILTERS) 
+ESP32CAN::ESP32CAN() : CAN_COMMON(BI_NUM_FILTERS)
 {
     for (int i = 0; i < BI_NUM_FILTERS; i++)
     {
@@ -169,19 +173,21 @@ int ESP32CAN::_setFilter(uint32_t id, uint32_t mask, bool extended)
 {
     for (int i = 0; i < BI_NUM_FILTERS; i++)
     {
-        if (!filters[i].configured) 
+        if (!filters[i].configured)
         {
             _setFilterSpecific(i, id, mask, extended);
             return i;
         }
     }
-    if (debuggingMode) Serial.println("Could not set filter!");
+    if (debuggingMode)
+        Serial.println("Could not set filter!");
     return -1;
 }
 
 void ESP32CAN::_init()
 {
-    if (debuggingMode) Serial.println("Built in CAN Init");
+    if (debuggingMode)
+        Serial.println("Built in CAN Init");
     for (int i = 0; i < BI_NUM_FILTERS; i++)
     {
         filters[i].id = 0;
@@ -190,13 +196,14 @@ void ESP32CAN::_init()
         filters[i].configured = false;
     }
 
-    if (!initializedResources) {
-                                 //Queue size, item size
-        CAN_cfg.rx_queue = xQueueCreate(rxBufferSize,sizeof(CAN_frame_t));
-        CAN_cfg.tx_queue = xQueueCreate(txBufferSize,sizeof(CAN_frame_t));
+    if (!initializedResources)
+    {
+        // Queue size, item size
+        CAN_cfg.rx_queue = xQueueCreate(rxBufferSize, sizeof(CAN_frame_t));
+        CAN_cfg.tx_queue = xQueueCreate(txBufferSize, sizeof(CAN_frame_t));
         callbackQueue = xQueueCreate(16, sizeof(CAN_FRAME));
         CAN_initRXQueue();
-                  //func        desc    stack, params, priority, handle to task
+        // func        desc    stack, params, priority, handle to task
         xTaskCreate(&task_CAN, "CAN_RX", 8192, this, 15, NULL);
         xTaskCreatePinnedToCore(&task_LowLevelRX, "CAN_LORX", 4096, this, 19, NULL, 1);
         xTaskCreatePinnedToCore(&CAN_WatchDog_Builtin, "CAN_WD_BI", 2048, this, 10, NULL, 1);
@@ -215,7 +222,7 @@ uint32_t ESP32CAN::init(uint32_t ul_baudrate)
 
 uint32_t ESP32CAN::beginAutoSpeed()
 {
-    const uint32_t bauds[7] = {1000,500,250,125,800,80,33}; //list of speeds to try, scaled down by 1000x
+    const uint32_t bauds[7] = {1000, 500, 250, 125, 800, 80, 33}; // list of speeds to try, scaled down by 1000x
     bool oldLOM = CAN_GetListenOnlyMode();
 
     _init();
@@ -225,14 +232,14 @@ uint32_t ESP32CAN::beginAutoSpeed()
     for (int i = 0; i < 7; i++)
     {
         CAN_cfg.speed = (CAN_speed_t)bauds[i];
-        CAN_stop(); //stop hardware so we can reconfigure it
+        CAN_stop(); // stop hardware so we can reconfigure it
         needReset = 0;
         faulted = false;
         Serial.print("Trying Speed ");
         Serial.print(bauds[i] * 1000);
-        CAN_init(); //set it up
-        delay(600); //wait a while
-        if (cyclesSinceTraffic < 2) //only would happen if there had been traffic
+        CAN_init();                 // set it up
+        delay(600);                 // wait a while
+        if (cyclesSinceTraffic < 2) // only would happen if there had been traffic
         {
             CAN_stop();
             CAN_SetListenOnly(oldLOM);
@@ -244,7 +251,6 @@ uint32_t ESP32CAN::beginAutoSpeed()
         {
             Serial.println(" FAILED.");
         }
-        
     }
     Serial.println("None of the tested CAN speeds worked!");
     CAN_stop();
@@ -280,26 +286,28 @@ void ESP32CAN::disable()
     faulted = false;
 }
 
-//This function is too big to be running in interrupt context. Refactored so it doesn't.
+// This function is too big to be running in interrupt context. Refactored so it doesn't.
 bool ESP32CAN::processFrame(CAN_frame_t &frame)
 {
     CANListener *thisListener;
     CAN_FRAME msg;
 
-    cyclesSinceTraffic = 0; //reset counter to show that we are receiving traffic
+    cyclesSinceTraffic = 0; // reset counter to show that we are receiving traffic
 
     msg.id = frame.MsgID;
     msg.length = frame.FIR.B.DLC;
     msg.rtr = frame.FIR.B.RTR;
     msg.extended = frame.FIR.B.FF;
-    for (int i = 0; i < 8; i++) msg.data.byte[i] = frame.data.u8[i];
-    
+    for (int i = 0; i < 8; i++)
+        msg.data.byte[i] = frame.data.u8[i];
+
     for (int i = 0; i < BI_NUM_FILTERS; i++)
     {
-        if (!filters[i].configured) continue;
+        if (!filters[i].configured)
+            continue;
         if ((msg.id & filters[i].mask) == filters[i].id && (filters[i].extended == msg.extended))
         {
-            //frame is accepted, lets see if it matches a mailbox callback
+            // frame is accepted, lets see if it matches a mailbox callback
             if (cbCANFrame[i])
             {
                 msg.fid = i;
@@ -321,34 +329,35 @@ bool ESP32CAN::processFrame(CAN_frame_t &frame)
                     thisListener = listener[listenerPos];
                     if (thisListener != NULL)
                     {
-                        if (thisListener->isCallbackActive(i)) 
-				        {
-					        msg.fid = 0x80000000ul + (listenerPos << 24ul) + i;
+                        if (thisListener->isCallbackActive(i))
+                        {
+                            msg.fid = 0x80000000ul + (listenerPos << 24ul) + i;
                             if (xQueueSend(callbackQueue, &msg, 0) != pdTRUE)
                                 rxProcQueueFull++;
                             return true;
-				        }
-				        else if (thisListener->isCallbackActive(numFilters)) //global catch-all 
-				        {
+                        }
+                        else if (thisListener->isCallbackActive(numFilters)) // global catch-all
+                        {
                             msg.fid = 0x80000000ul + (listenerPos << 24ul) + 0xFF;
                             if (xQueueSend(callbackQueue, &msg, 0) != pdTRUE)
                                 rxProcQueueFull++;
                             return true;
-				        }
+                        }
                     }
                 }
             }
-            
-            //otherwise, send frame to input queue
+
+            // otherwise, send frame to input queue
             xQueueSend(CAN_cfg.rx_queue, &frame, 0);
-            if (debuggingMode) Serial.write('_');
+            if (debuggingMode)
+                Serial.write('_');
             return true;
         }
     }
     return false;
 }
 
-bool ESP32CAN::sendFrame(CAN_FRAME& txFrame)
+bool ESP32CAN::sendFrame(CAN_FRAME &txFrame)
 {
     CAN_frame_t __TX_frame;
 
@@ -356,49 +365,68 @@ bool ESP32CAN::sendFrame(CAN_FRAME& txFrame)
     __TX_frame.FIR.B.DLC = txFrame.length;
     __TX_frame.FIR.B.RTR = (CAN_RTR_t)txFrame.rtr;
     __TX_frame.FIR.B.FF = (CAN_frame_format_t)txFrame.extended;
-    for (int i = 0; i < 8; i++) __TX_frame.data.u8[i] = txFrame.data.byte[i];
+    for (int i = 0; i < 8; i++)
+        __TX_frame.data.u8[i] = txFrame.data.byte[i];
 
-    if (CAN_TX_IsBusy()) //hardware already sending, queue for sending when possible
-    {
-        xQueueSend(CAN_cfg.tx_queue,&__TX_frame,0);
-        if (debuggingMode) Serial.write('<');
+    uint32_t txQueueAvailTmp = uxQueueSpacesAvailable(CAN_cfg.tx_queue);
+    if (txQueueAvailTmp < txQueueAvail) {
+        txQueueAvail = txQueueAvailTmp;
+        Serial.println("-------------------- txQueueAvail = " + (String)txQueueAvail);
     }
-    else //hardware is free, send immediately
+
+    if (CAN_TX_IsBusy()) // hardware already sending, queue for sending when possible
+    {
+        xQueueSend(CAN_cfg.tx_queue, &__TX_frame, 0);
+        if (debuggingMode)
+            Serial.write('<');
+    }
+    else // hardware is free, send immediately
     {
         CAN_write_frame(&__TX_frame);
-        if (debuggingMode) Serial.write('>');
+        if (debuggingMode)
+            Serial.write('>');
     }
     return true;
 }
 
 bool ESP32CAN::rx_avail()
 {
-    if (!CAN_cfg.rx_queue) return false;
-    return uxQueueMessagesWaiting(CAN_cfg.rx_queue) > 0?true:false;
+    if (!CAN_cfg.rx_queue)
+        return false;
+    return uxQueueMessagesWaiting(CAN_cfg.rx_queue) > 0 ? true : false;
 }
 
 uint16_t ESP32CAN::available()
 {
-    if (!CAN_cfg.rx_queue) return 0;
+    if (!CAN_cfg.rx_queue)
+        return 0;
     return uxQueueMessagesWaiting(CAN_cfg.rx_queue);
 }
 
 uint32_t ESP32CAN::get_rx_buff(CAN_FRAME &msg)
 {
+    
+    uint32_t rxQueueAvailTmp = uxQueueSpacesAvailable(CAN_cfg.rx_queue);
+    if (rxQueueAvailTmp < rxQueueAvail) {
+        rxQueueAvail = rxQueueAvailTmp;
+        Serial.println("-------------------- rxQueueAvail = " + (String)rxQueueAvail);
+    }
+
+
     CAN_frame_t __RX_frame;
-    //receive next CAN frame from queue
-    if(xQueueReceive(CAN_cfg.rx_queue,&__RX_frame, 0)==pdTRUE)
+    // receive next CAN frame from queue
+    if (xQueueReceive(CAN_cfg.rx_queue, &__RX_frame, 0) == pdTRUE)
     {
         msg.id = __RX_frame.MsgID;
         msg.length = __RX_frame.FIR.B.DLC;
         msg.rtr = __RX_frame.FIR.B.RTR;
         msg.extended = __RX_frame.FIR.B.FF;
-        for (int i = 0; i < 8; i++) msg.data.byte[i] = __RX_frame.data.u8[i];
+        for (int i = 0; i < 8; i++)
+            msg.data.byte[i] = __RX_frame.data.u8[i];
         return true;
     }
     return false;
 }
-
 
 uint8_t ESP32CAN::hasRxFault2()
 {
